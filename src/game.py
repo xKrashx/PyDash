85a137585d8718b8cac5db1bb64e890f91a0c818
xKrashx <hristokanevkrash@gmail.com>
2025-09-16 23:05:58 +0300

    Adding a player class

--- src/game.py
+++ src/game.py
@@ -2,6 +2,7 @@ import pygame
 
 from src.images import PLAYER_IMAGE
 from src.constants import RED, SCREEN_SIZE
+from src.player import Player
 
 class Game:
     def __init__(self):
@@ -11,19 +12,34 @@ class Game:
         pygame.display.set_caption('Pydash: Geometry Dash in Python')
         pygame.display.set_icon(PLAYER_IMAGE)
 
+        self.player_sprite = pygame.sprite.Group()
+
         self.clock = pygame.time.Clock()
+        self.player = Player(self.player_sprite)
 
     def run(self):
         done = False
+        angle = 0
 
         while not done:
-            alpha_surf = pygame.Surface(self.screen.get_size())
-            alpha_surf.fill(RED)
-
-            self.screen.blit(alpha_surf, (0, 0))
-
             for event in pygame.event.get():
                 done = event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE)
+
+            keys = pygame.key.get_pressed()
+
+            if keys[pygame.K_SPACE]: self.player.jump()
+
+            self.screen.fill(RED)
+            self.player.draw_particle_trail(self.screen)
+
+            self.player_sprite.update()
+
+            if self.player.is_jumping:
+                angle -= 8.1712
+                self.player.blitRotate(self.screen, angle)
+            else:
+                self.player_sprite.draw(self.screen)
+
 
             pygame.display.flip()
             self.clock.tick(60)
---
+++ src/player.py
@@ -0,0 +1,65 @@
+import pygame
+import random
+
+from pygame.math import Vector2
+
+from src.constants import WHITE, GRAVITY, MAX_VELOCITY, SCREEN_SIZE
+from src.images import PLAYER_IMAGE
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, *groups):
+        super().__init__(*groups)
+        self.position = [66, 0]
+
+        self.image = pygame.transform.smoothscale(PLAYER_IMAGE, (32, 32))
+        self.rect = self.image.get_rect(center=self.position)
+        self.jump_amount = 10
+        self.particles = []
+        self.is_jumping = False
+        self.vel = Vector2(6, 0)
+
+    def draw_particle_trail(self, surface: pygame.Surface, color=WHITE):
+        self.particles.append([
+            [self.rect.left - 6, self.rect.bottom - 6], # position
+            [random.randint(0, 25) // 10 - 1, 0],       # velocity
+            random.randint(5, 8)                        # size/life
+        ])
+
+        for particle in self.particles:
+            particle[0][0] += particle[1][0]
+            particle[0][1] += particle[1][1]
+            particle[2] -= 0.2
+            particle[1][0] -= 0.1
+            pygame.draw.rect(surface, color, ([particle[0][0], particle[0][1]], [particle[2], particle[2]]))
+            if particle[2] <= 0: self.particles.remove(particle)
+
+    def jump(self):
+        if self.is_jumping: return
+        self.vel.y = -self.jump_amount
+        self.is_jumping = True
+
+    def update(self):
+        self.vel.y = min(self.vel.y + GRAVITY, MAX_VELOCITY)
+
+        self.rect.top += self.vel.y
+        # TODO: Remove this cap is only for testing
+        if self.rect.top >= SCREEN_SIZE[1] - self.rect.height:
+            self.is_jumping = False
+            self.rect.top = SCREEN_SIZE[1] - self.rect.height
+
+    def blitRotate(self, surface: pygame.Surface, angle: float):
+        origin_position = (16, 16)
+        width, height = self.image.get_size()
+        box = [Vector2(point) for point in [(0, 0), (width, 0), (width, -height), (0, -height)]]
+        box_rotate = [point.rotate(angle) for point in box]
+
+        min_box = (min(box_rotate, key=lambda point: point[0])[0], min(box_rotate, key=lambda point: point[1])[1])
+        max_box = (max(box_rotate, key=lambda point: point[0])[0], max(box_rotate, key=lambda point: point[1])[1])
+
+        pivot = Vector2(origin_position[0], -origin_position[1])
+        pivot_rotate = pivot.rotate(angle)
+        pivot_move = pivot_rotate - pivot
+
+        origin = (self.rect.center[0] - origin_position[0] + min_box[0] - pivot_move[0], self.rect.center[1] - origin_position[1] - max_box[1] + pivot_move[1])
+        rotated_image = pygame.transform.rotozoom(self.image, angle, 1)
+        surface.blit(rotated_image, origin)
