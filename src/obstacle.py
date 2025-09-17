b4188131cdf05e5c75b71ad38833a636d62f0e28
xKrashx <hristokanevkrash@gmail.com>
2025-09-17 20:24:47 +0300

    Adding obstacle class

---
+++ src/obstacle.py
@@ -0,0 +1,7 @@
+import pygame
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, image, pos, *groups):
+        super().__init__(*groups)
+        self.image = image
+        self.rect = self.image.get_rect(topleft=pos)
